a21cecc6f635b17e45bbd913d6835ffb8a5c32b7
dan-gryy <dan.gryschenko@gmail.com>
2025-09-29 00:45:32 +0300

    code formatted

--- lab_7/remove.js
+++ lab_7/remove.js
@@ -1,10 +1,18 @@
 //Функція видаляє обраний елемент в масиві
 
-let array = ['Kiev', 'Beijing', 'Lima', 'Saratov', 'Lviv', 'New York', 'barcelona', 'London' ];
-const removeElements = (arr, item) => {
-    return arr.filter(element => element !== item)
-}
-array = removeElements(array, 'Saratov') // Видаляє місто Саратов
-array = removeElements(array, 'Paris') // Не видаляє Париж, адже його немає в масиві 
+let array = [
+  "Kiev",
+  "Beijing",
+  "Lima",
+  "Saratov",
+  "Lviv",
+  "New York",
+  "barcelona",
+  "London",
+];
+const removeElement = (arr, item) => {
+  return arr.filter((element) => element !== item);
+};
+array = removeElement(array, "Saratov"); // Видаляє місто Саратов
+array = removeElement(array, "Paris"); // Не видаляє Париж, адже його немає в масиві
 console.log(array);
-
