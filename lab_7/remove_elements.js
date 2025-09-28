492cfa84b16956ffcde47390e19c06eb7642f1a2
dan-gryy <dan.gryschenko@gmail.com>
2025-09-29 02:14:29 +0300

    small changes

--- lab_7/remove_elements.js
+++ lab_7/remove_elements.js
@@ -7,12 +7,12 @@ let array = [
   "Saratov",
   "Lviv",
   "New York",
-  "barcelona",
+  "Barcelona",
   "London",
 ];
-const removeElement = (arr, item) => {
-  return arr.filter(element => element !== item);
+const removeElement = (arr, items) => {
+  return arr.filter((element) => !items.includes(element));
 };
-array = removeElement(array, "Saratov"); // Видаляє місто Саратов
+array = removeElement(array, ["Saratov", "London", "Lviv", "Barcelona"]); // Видаляє місто Саратов, Лондон, Львів та Барселону
 array = removeElement(array, "Paris"); // Не видаляє Париж, адже його немає в масиві
 console.log(array);
