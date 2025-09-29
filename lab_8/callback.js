6b976bef79196dfa0450a1677fd52126fe4c4b1d
dan-gryy <dan.gryschenko@gmail.com>
2025-09-29 17:23:23 +0300

    small changes

--- lab_8/callback.js
+++ lab_8/callback.js
@@ -1,3 +1,5 @@
+//Функція iterate приймає об'єкт і функцію зворотного виклику (callback). Вона ітерує ключі об'єкта і викликає callback для кожної пари ключ-значення.
+
 const obj = { a: 1, b: 2, c: 3 };
 function iterate(obj, callback) {
   for (let key in obj) {
--- lab_8/closure.js
+++ lab_8/closure.js
@@ -1,3 +1,5 @@
+//Функція store приймає значення і повертає функцію, яка при виклику повертає це значення.
+
 const read = store(5);
 const value = read();
 function store(value) {
