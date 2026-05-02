ef42b09c6beeae29a5a87234b62ab60a42d431fb
dan-gryy <dan.gryschenko@gmail.com>
2026-05-02 23:16:38 +0300

    Refactor file processing function for improved readability and add sample data file

--- Second semester/lab6/lab6.js
+++ Second semester/lab6/lab6.js
@@ -1,17 +1,32 @@
-const fs = require('fs');
-const readline = require('readline');
+const fs = require("fs");
+const readline = require("readline");
 
 async function processFile(pathToFile) {
-    const stream = fs.createReadStream(pathToFile, { encoding: 'utf-8' });
+  const stream = fs.createReadStream(pathToFile, { encoding: "utf-8" });
 
-    const rl = readline.createInterface({
-        input: stream,
-        crlfDelay: Infinity
-    });
+  const rl = readline.createInterface({
+    input: stream,
+    crlfDelay: Infinity,
+  });
 
-    for await (let line of rl) {
-        console.log(line);
+  let totalLines = 0;
+  let totalWords = 0;
+  let longLines = 0;
+
+  for await (let line of rl) {
+    totalLines++;
+
+    if (line.length > 20) {
+      longLines++;
     }
+
+    const words = line.trim().split(/\s+/);
+    totalWords += words.filter((w) => w.length > 0).length;
+  }
+
+  console.log("Lines:", totalLines);
+  console.log("Words:", totalWords);
+  console.log("Long lines:", longLines);
 }
 
-processFile('./data.txt');
+processFile(__dirname + '/data.txt');
