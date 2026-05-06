bda8e93b66c51fa5ebaff95f666ac1ad50ecb9f3
dan-gryy <dan.gryschenko@gmail.com>
2026-05-06 23:39:18 +0300

    Add leave method to User class and log chat activity

--- Second semester/lab7/lab7.js
+++ Second semester/lab7/lab7.js
@@ -25,6 +25,11 @@ class User {
   send(text) {
     this.chat.sendMessage(this.name, text);
   }
+
+  leave() {
+    this.chat.off("message", this.listener);
+    console.log(this.name + " left the chat");
+  }
 }
 
 const chat = new Chat();
@@ -32,5 +37,13 @@ const chat = new Chat();
 const userA = new User("Ilya", chat);
 const userB = new User("Daniil", chat);
 
+chat.on("message", (data) => {
+  console.log("[chat activity] " + data.user + " says: " + data.text);
+});
+
 userA.send("Hello!");
 userB.send("Hi!");
+
+userB.leave();
+
+userA.send("Are you still here?");
