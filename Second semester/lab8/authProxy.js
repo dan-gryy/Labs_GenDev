be5e4a8d90aa47f2a27e3363261bb5f863abd16e
dan-gryy <dan.gryschenko@gmail.com>
2026-05-14 15:52:36 +0300

    Enhance AuthProxy with request limit and retry logic; update response handling in index.js

--- Second semester/lab8/authProxy.js
+++ Second semester/lab8/authProxy.js
@@ -4,9 +4,22 @@ class AuthProxy {
   constructor(type, value) {
     this.type = type;
     this.value = value;
+    this.requests = 0;
+    this.limit = 5;
+  }
+
+  changeAuth(type, value) {
+    this.type = type;
+    this.value = value;
   }
 
   async send(url, method = "GET", data = null) {
+    if (this.requests >= this.limit) {
+      throw new Error("Too many requests");
+    }
+
+    this.requests++;
+
     const headers = {};
 
     if (this.type === "apiKey") {
@@ -20,14 +33,35 @@ class AuthProxy {
     if (this.type === "oauth") {
       headers["Authorization"] = `OAuth ${this.value}`;
     }
-    const response = await axios({
-      url,
-      method,
-      data,
-      headers,
-    });
 
-    return response.data;
+    console.log(`${method} request to ${url}`);
+
+    try {
+      const response = await axios({
+        url,
+        method,
+        data,
+        headers,
+      });
+
+      return response.data;
+    } catch (error) {
+      if (error.response && error.response.status === 401) {
+        this.value = "new-token-456";
+        headers["Authorization"] = `Bearer ${this.value}`;
+
+        const retryResponse = await axios({
+          url,
+          method,
+          data,
+          headers,
+        });
+
+        return retryResponse.data;
+      }
+
+      throw error;
+    }
   }
 }
 
--- Second semester/lab8/index.js
+++ Second semester/lab8/index.js
@@ -9,15 +9,22 @@ app.get("/posts", async (req, res) => {
   try {
     const data = await proxy.send("https://jsonplaceholder.typicode.com/posts");
 
-    res.json(data.slice(0, 5));
+    res.status(200).json(data.slice(0, 5));
   } catch (error) {
     res.status(500).json({
-      message: "Request error",
+      message: error.message,
     });
   }
+});
+
+app.get("/change-auth", (req, res) => {
+  proxy.changeAuth("apiKey", "api-key-111");
+
+  res.json({
+    message: "Auth changed",
+  });
 });
 
 app.listen(3000, () => {
   console.log("Server started on port 3000");
 });
-    
